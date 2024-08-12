4f632dc385c82d3663ff255cc9e95f9a15b67b63
keerti-lab <keerti.gdev@gmail.com>
2024-08-12 19:46:48 +0530

    repo

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 #declaring of variables
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=$1
+PERSON2=$2
+
 #refering of variables
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
