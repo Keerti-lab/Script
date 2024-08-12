b8110714a0855348f9922aae99f16a8eb8ac69cb
keerti-lab <keerti.gdev@gmail.com>
2024-08-12 20:25:04 +0530

    repo

---
+++ 07-spl-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "ALL Variables: $@"
+echo "Number of variables passed: $#"
+echo "Script  Name: $0"
+echo "Current working directory: $PWD"
+echo "Home of Current user: $HOME"
+echo "Which user is running the script: $USER"
